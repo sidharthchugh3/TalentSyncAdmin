45471e8cd1480542bba4664d97e882c859e8b45f
Anil Suthar <asuthar2002@gmail.com>
2025-09-25 15:29:50 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";   // <-- import Provider
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store.jsx"
 import App from "./App.jsx";
-import { store } from "./store/store";    // <-- import store
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>   {/* <-- wrap App with Provider */}
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,15 +1,38 @@
-// src/store/store.js
+// src/store/store.jsx
 import { configureStore } from "@reduxjs/toolkit";
 import companyReducer from "./slices/CompanySlice";
-import authReducer from "./slices/authSlice"
-import jobReducer from "./slices/JobsSlice"
-import recruiterReducer from "./slices/recruiterSlice"
+import authReducer from "./slices/authSlice";
+import jobReducer from "./slices/JobsSlice";
+import recruiterReducer from "./slices/recruiterSlice";
 
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+
+// --- Persist Config for auth slice ---
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["user", "accessToken", "isEmailVerified"],
+};
+
+// --- Persisted reducer ---
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+
+// --- Configure store ---
 export const store = configureStore({
   reducer: {
     company: companyReducer,
-    auth: authReducer,
+    auth: persistedAuthReducer, // use persisted reducer here
     jobs: jobReducer,
     recruiter: recruiterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// --- Persistor ---
+export const persistor = persistStore(store);
